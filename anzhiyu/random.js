6fd29e40fc20baab25ff2018afab3f6175cec39a
qingyukb <961064865@qq.com>
2025-03-15 11:54:17 +0800

    Site updated: 2025-03-15 11:54:12

--- anzhiyu/random.js
+++
@@ -1,1 +0,0 @@
-var posts=["undefined/4a17b156.html","undefined/8d48c6a1.html","undefined/c6c552a3.html","undefined/7a776471.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
