4eb407fb65b1846dee5ce44f3aac6b7612da4995
qingyukb <961064865@qq.com>
2025-11-02 12:39:14 +0800

    Site updated: YYYY-11-2025年11月2日 12:39:13

---
+++ anzhiyu/random.js
@@ -0,0 +1,56 @@
+var posts=["posts/4512133.html","posts/8331de3d.html","posts/acc595f5.html","posts/8d48c6a1.html","posts/7a776471.html","posts/3d85b155.html","posts/c6c552a3.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };var friend_link_list=[{"name":"Hexo","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"anzhiyu主题","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg","color":"vip","tag":"技术"},{"name":"Fomalhaut🥝","link":"https://www.fomal.cc/","avatar":"https://www.fomal.cc/assets/avatar.webp","descr":"Future is now 🍭🍭🍭","siteshot":"https://source.fomal.cc/siteshot/www.fomal.cc.webp"},{"name":"青桔气球","link":"https://blog.qjqq.cn/","avatar":"https://q2.qlogo.cn/headimg_dl?dst_uin=1645253&spec=640","descr":"分享网络安全与科技生活","siteshot":"https://q2.qlogo.cn/headimg_dl?dst_uin=1645253&spec=640"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","recommend":true},{"name":"Fomalhaut🥝","link":"https://www.fomal.cc/","avatar":"https://www.fomal.cc/assets/avatar.webp","descr":"Future is now 🍭🍭🍭","recommend":true},{"name":"葱苓小筑","link":"https://blog.ciraos.top/","avatar":"https://mx.ciraos.top/api/v2/objects/avatar/ufh295h43mly31gmw0.avif","descr":"a small blog station."},{"name":"陆小启","link":"https://blog.zonox.top/","avatar":"https://bu.dusays.com/2025/03/14/67d3fbedaa1c6.webp","descr":"往者不谏，来者可追","siteshot":"https://bu.dusays.com/2025/07/27/68861f5469c6b.png"},{"name":"王权富贵","link":"https://blog.w1f.top/","avatar":"https://tu.w1f.top/file/1754042909789_favicon.png","descr":"岁月温柔，万物生长。","siteshot":"https://tu.w1f.top/file/1754228138583_image.png"},{"name":"青桔气球","link":"https://blog.qjqq.cn/","avatar":"https://q2.qlogo.cn/headimg_dl?dst_uin=1645253&spec=640","descr":"分享网络安全与科技生活","siteshot":"https://q2.qlogo.cn/headimg_dl?dst_uin=1645253&spec=640","recommend":true},{"name":"小亮的部落阁","link":"https://blog.752132.xyz/","avatar":"https://z.wiki/u/KqIZzi","descr":"欲望以提升热忱，毅力以磨平高山","siteshot":"https://z.wiki/u/2cXqKC"},{"name":"卖桃翁小窝","link":"https://ayakasuki.com","avatar":"https://tc.ayakasuki.com/a/2023/10/14/biji6529e8a16b6d7.png","descr":"一切向好，生活向阳","siteshot":"https://tc.ayakasuki.com/a/2025/06/11/biji68493e2b62bd0.png"},{"name":"HugeH4rD-技术苑","link":"https://blog.806006.xyz/","avatar":"https://blog.806006.xyz/img/avatar.png","descr":"浮舟沧海，立马昆仑✨","siteshot":"https://npm.elemecdn.com/browse/hexo-lib@1.0.22/img/webcut.webp"},{"name":"东方月初","link":"https://blog.biuxin.com/","avatar":"https://x.xinb.de/i/2024/09/19/040857.webp","descr":"分享有趣但又无聊的东西。","siteshot":"https://x.xinb.de/i/2024/09/23/930491.webp"},{"name":"Elykia","link":"https://blog.elykia.cn/","avatar":"https://bu.dusays.com/2024/10/25/671b2438203a6.gif","descr":"致以无暇之人","siteshot":"https://bu.dusays.com/2024/10/25/671b50dc2dd2b.png"},{"name":"梦爱吃鱼","link":"https://blog.bsgun.cn/","avatar":"https://oss-cdn.bsgun.cn/logo/avatar.256.png","descr":"不负心灵，不负今生。"},{"name":"大爱无痕","link":"https://blog.youngboy.top/","avatar":"https://tu.youngboy.top/api/file/21447ce1fabc06c31aa18.jpg","descr":"心灵的栖居地，记录点滴生活","siteshot":"https://pic.superbed.cc/item/679dca48fa9f77b4dc57180d.jpg"},{"name":"辣条","link":"https://blog.owwo.fun","descr":"悠远的天空，在苍穹的尽头","avatar":"https://blog.owwo.fun/img/avatar.jpg"},{"name":"胜天半子","link":"https://blog.291055.xyz/","avatar":"https://i1.wp.com/ruom.wuaze.com/i/2024/11/02/166823.webp","descr":"事已至此，先吃饭吧😋","siteshot":"https://i1.wp.com/ruom.wuaze.com/i/2024/11/08/350149.webp"},{"name":"周润发","link":"https://blog.aizrf.com/","avatar":"https://blog.aizrf.com/img/logo.png","descr":"不忘初心，方得始终","siteshot":"https://blog.aizrf.com/img/web_img.png"}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
